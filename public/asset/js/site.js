31712da18668473e343e3ee01d019ef1dcbe2ea4
Denis Anikin <sizz00@gmail.com>
2018-11-20 17:04:10 +0400

    dev

--- public/asset/js/site.js
+++ public/asset/js/site.js
@@ -1,7 +1,24 @@
 $(function(){
     
-    $('ul.navbar-nav a.nav-link').click(function () {
+    $('ul.navbar-nav a.nav-link').click(function (event) {
 
+        var s = $(this).attr('href');
+
+        var o = $(s);
+
+        if (o.length > 0) {
+            var x = o.position().top;
+
+            $('html, body').stop().animate({scrollTop: x}, 1000);
+
+            event.preventDefault();
+        }
+
+    })
+
+
+    $('#collapseSertifikaty').collapse({
+        toggle: false
     })
     
     
